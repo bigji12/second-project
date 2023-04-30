5f71185faa8524dcc14ef1bae4788eb288185a50
bigji12 <dritoni_112@live.com>
2023-04-30 23:16:30 +0200

    bigji

--- UserInterface.java
+++ UserInterface.java
@@ -15,8 +15,28 @@ public class UserInterface{
             System.out.println("Welcome here where the food is the best in the world");
             System.out.println("What can we offer you?(Drinks or Food)");
             String whatCosumatorWant = scan.nextLine();
-            if(whatCosumatorWant.equalsIgnoreCase("food")){
+            if(whatCosumatorWant.equals("food")){
+                foodMethod();
+            }
+
+        }
+    }
+    
+    public void foodMethod() {
+        products.printMenu();
+        System.out.println("Everything is very delicious.");
+        System.out.println("What do you like to eat?");
+        String food = scan.nextLine();
+        if(products.contanes(food)){
+            System.out.println("Bill you have to pay for your food: " + products.priceOfFood);
+            System.out.println("Do you want something else?");
+            String somethingElse = scan.nextLine();
+            if(something.equals("i won somthing else to eat")){
                 products.printMenu();
+                String secondFood = scan.nextLine();
+                if(products.contanes(secondFood)){
+                    System.out.println("Your total bill it's: " products.totalPrice);
+                }
             }
 
         }
