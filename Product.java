e193f4c90ba1aa10e2c827f0f47f97a175769c40
bigji12 <dritoni_112@live.com>
2023-04-30 14:48:34 +0200

    product

--- Product.java
+++ Product.java
@@ -1,33 +1,33 @@
 
 public class Product{
-    private String name;
-    private double price;
+    private String foodName;
+    private double foodPrice;
 
-    public Product(String name, double price) {
-        this.name = name;
-        this.price = price;
+    public Product(String foodName, double foodPrice) {
+        this.foodName = foodName;
+        this.foodPrice = foodPrice;
     }
 
     public String getName() {
-        return name;
+        return this.foodName; //Merr emrin e produktit(ushqimit)
     }
 
     public double getPrice() {
-        return price;
+        return this.foodPrice; //Merr qmimin e produktit(ushqimit)
     }
 
     // Metoda për të marrë informacionin e produkteve nga file-i
     public static Product[] readProductsFromFile(String fileName) {
-        try ( Scanner scan = new Scanner(Paths.get(fileName))) {
+        try ( Scanner scan = new Scanner(Paths.get())) {
             while (scan.hasNextLine()) {
-                String line = scanner.nextLine();
+                String line = scan.nextLine();
                 String[] parts = line.split(",");
                 String name = parts[0];
                 double price = Double.parseDouble(parts[1]);
                 products[i] = new Product(name, price);
                 i++;
             }
-            
+
         } catch (Exception e) {
 
         }
