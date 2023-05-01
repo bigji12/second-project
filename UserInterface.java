cd1a8a889b025fa00c8d2f13f30b2631975838af
EgzonOsmani <oegzon73@gmail.com>
2023-04-30 23:18:57 -0700

    Egzon

--- UserInterface.java
+++ UserInterface.java
@@ -1,24 +1,43 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-public class UserInterface{
+
+public class UserInterface {
     private Scanner scan;
-    private ArrayList <Product>  products;
-    private Product product;
-    public UserInterface(Scanner scan,Product product){
+    private ArrayList<Product> products;
+
+    public UserInterface(Scanner scan) {
         this.scan = scan;
         this.products = new ArrayList<>();
-        this.product = product;
     }
 
-    public void start(){
-        while(true){
+    public void start() {
+        while (true) {
             System.out.println("Hi there!");
             System.out.println("Welcome here where the food is the best in the world");
             System.out.println("What can we offer you?(Drinks or Food)");
             String whatCosumatorWant = scan.nextLine();
-            if(whatCosumatorWant.equals("food")){
-                this.product.iteratorInFile("Menu");
-                this.product.printMenu();
+            if (whatCosumatorWant.equals("food")) {
+                foodMethod();
+            }
+
+        }
+    }
+
+    public void foodMethod() {
+        products.printMenu();
+        System.out.println("Everything is very delicious.");
+        System.out.println("What do you like to eat?");
+        String food = scan.nextLine();
+        if(products.contanes(food)){
+            System.out.println("Bill you have to pay for your food: " + products.priceOfFood);
+            System.out.println("Do you want something else?");
+            String somethingElse = scan.nextLine();
+            if(something.equals("i won somthing else to eat")){
+                products.printMenu();
+                String secondFood = scan.nextLine();
+                if(products.contanes(secondFood)){
+                    System.out.println("Your total bill it's: " products.totalPrice);
+                }
             }
 
         }
